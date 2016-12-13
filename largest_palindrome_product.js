cb197dd1a61d3e048efac804a6d00478d60c2af9
Tyler Nichols <tylernchls@gmail.com>
2016-12-13 11:03:46 -1000

    finished

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,18 +5,58 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+module.exports = function largestPalindrome(digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
+  var factorCheck;
+  var productArr = [];
+  var from;
+  var to;
 
-  // do your work here
+  if (digits === 2) {
+    from = 10;
+    to = 99;
+  } else {
+    from = 100;
+    to = 999;
+  }
+
+  for (var i = from; i < to; i++) {
+    for (var x = i +1; x <= to; x++) {
+      factorCheck = i * x;
+      if (isPalindrome(factorCheck)) {
+    factor._0 = i;
+    factor._1 = x;
+    productArr.push(factorCheck);
+
+  }
+  }
+  }
+
+  palindromeNumber = getMaxOfArray(productArr);
 
 
-
-  return {
+    return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
   };
 };
+
+function isPalindrome(num) {
+  var numReverse = num.toString().split("").reverse().join("");
+  numReverse = Number(numReverse);
+
+  if (num === numReverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+function maxOfArray(numArr) {
+  return Math.max(apply(null, numArr));
+}
